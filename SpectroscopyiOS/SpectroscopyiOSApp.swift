9a0b83570bdef095c997d20f49748324e57b243f
Olivia Rorke <ororke1@gulls.salisbury.edu>
2021-09-20 12:38:28 -0700

    First commit

---
+++ SpectroscopyiOS/SpectroscopyiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpectroscopyiOSApp.swift
+//  SpectroscopyiOS
+//
+//  Created by Olivia Rorke on 9/20/21.
+//
+
+import SwiftUI
+
+@main
+struct SpectroscopyiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
