1e15c859778f72a0ca40b5a06676d06650c96fae
Cody Murrer <cmurrer1@ac102-02-00948.salisbury.edu>
2021-10-18 12:52:14 -0400

    setting up functions for when people push

--- SUChemistryiOSApp/ViewController.swift
+++ SUChemistryiOSApp/ViewController.swift
@@ -12,7 +12,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+// waiting for others to push code to start working on showing data on UI
+    // Deletes the current samples saved data or just clears the screen back to default if no data is saved
+    @objc
+    func deleteAction(){
+        
+    }
+    // Saves the current samples data(name,concentration, RGB, absorbance, wavelength, thumbnails)
+    @objc
+    func saveAction(){
+        
+    }
+    
+    func defaultValues(){
+        
+    }
+    
+    func updateValues(){
+        
+    }
+    
+    func deleteValues(){
+        
+    }
 }
 
