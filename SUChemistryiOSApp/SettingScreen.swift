b7e350015565e09e80e7e9201200d9f65cb12e1b
Daniel <dbradley1@ac102-01-00946.salisbury.edu>
2021-10-20 15:38:52 -0400

    commiting changes

--- SUChemistryiOSApp/SettingScreen.swift
+++ SUChemistryiOSApp/SettingScreen.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct SettingScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            VStack{
+                
+            }
+        }
     }
 }
 
