d8907911effa2aeeb572c153d295e607bfb22831
Lukademus <57402574+Lukademus@users.noreply.github.com>
2021-10-27 15:44:30 -0400

    corrected a mistaken commit

--- ViewController.swift
+++ ViewController.swift
@@ -1,181 +1,256 @@
 //
 //  ViewController.swift
-//  AVFCamera
+//  SUChemistryiOSApp
 //
 
-
-import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
     
-    //capture session
-    var session: AVCaptureSession?
-    //photo output
-    let output = AVCapturePhotoOutput()
-    //video preview
-    let previewLayer = AVCaptureVideoPreviewLayer()
-    //shutter button
-    private let shutterButton: UIButton = {
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-        button.layer.cornerRadius = 50
-        button.layer.borderWidth = 50
-        button.layer.borderColor = UIColor.white.cgColor
-        return button
-    }()
-
+    var button = dropDownBtn()
+// User input variables
+    var userConcentration:Double = 0.0
+    var userSampleName:String = ""
+    var userPathLength:Double = 0.0
+// Calculation Variables
+    var calcWavelength:Double = 0.0
+    var calcAbsorbance:Double = 0.0
+// RGB variables
+    var camRed:Int = 0
+    var camGreen:Int = 0
+    var camBlue:Int = 0
+// sample variables
+    var control = SampleData();
+    var sampleOne = SampleData()
+    var sampleTwo = SampleData()
+    var sampleThree = SampleData()
+    var sampleFour = SampleData()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .black
-        previewLayer.backgroundColor = UIColor.systemRed.cgColor
-        view.layer.addSublayer(previewLayer)
-        view.addSubview(shutterButton)
-        checkCameraPermissions()
+        // Do any additional setup after loading the view, typically from a nib.
         
-        shutterButton.addTarget(self, action: #selector(didTapTakePhoto), for: .touchUpInside)
+        
+        //Configure the button
+        button = dropDownBtn.init(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        button.setTitle("Colors", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        //Add Button to the View Controller
+        self.view.addSubview(button)
+        
+        //button Constraints
+        button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        //Set the drop down menu's options
+        button.dropView.dropDownOptions = ["Blue", "Green", "Magenta", "White", "Black", "Pink"]
+        
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+// waiting for others to push code to start working on showing data on UI
+    // Deletes the current samples saved data or just clears the screen back to default if no data is saved
+    var fileDir:String = ""
+    @objc
+    func deleteAction(){
+        deleteValues()
+        // using sample one as a temp variable for right nows
+        updateValues(samp: sampleOne)
+    }
+    // Saves the current samples data(name,concentration, RGB, absorbance, wavelength, thumbnails)
+    @objc
+    func saveAction(){
+        // looks for working directory
+        fileDir = "\(getFilePath())"
         
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        previewLayer.frame = view.bounds
-        
-        shutterButton.center = CGPoint(x: view.frame.size.width/2
-                                       , y: view.frame.size.height - 200)
+    func getFilePath()->URL{
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = path[0]
+        return documentsDirectory
     }
     
-    private func checkCameraPermissions(){
-        switch AVCaptureDevice.authorizationStatus(for: .video){
-        case .notDetermined:
-        //request
-            AVCaptureDevice.requestAccess(for: .video) { [weak self] granted in
-                guard granted else{
-                    return
-                }
-                DispatchQueue.main.async {
-                    self?.setUpCamera()
-                }
-            }
-        case .restricted:
-         break
-        case .denied:
-        break
-        case .authorized:
-            setUpCamera()
-        @unknown default:
-            break
-        }
-    }
-    private func setUpCamera(){
-        let session = AVCaptureSession()
-        if let device = AVCaptureDevice.default(for: .video) {
-            do {
-                let input = try AVCaptureDeviceInput(device: device)
-                if session.canAddInput(input){
-                    session.addInput(input)
-                }
-                if session.canAddOutput(output){
-                    session.addOutput(output)
-                }
-                
-                previewLayer.videoGravity = .resizeAspectFill
-                previewLayer.session = session
-                
-                session.startRunning()
-                self.session = session
-                
-            }
-            catch{
-                print(error)
-            }
-        }
+    func defaultValues(samp:SampleData){
+        sampleOne.setSampleName(name: "Sample One")
+        sampleTwo.setSampleName(name: "Sample Two")
+        sampleThree.setSampleName(name: "Sample Three")
+        sampleFour.setSampleName(name: "Sample Four")
+        
+        camRed = 0
+        camGreen = 0
+        camBlue = 0
+        
+        userConcentration = 0.0
+        userPathLength = 0.0
+     //   userSampleName = sampleOne.getSampleName()
     }
     
-    @objc private func didTapTakePhoto(){
-        output.capturePhoto(with: AVCapturePhotoSettings(),
-                            delegate: self)
+    func updateValues(samp:SampleData){
+        /*
+            Waiting for UI to get finished before puttin in major if statement
+            that tells the different sample objects to load data onto screen
+            
+            Makes use of the setters and getters
+         */
     }
     
-    /**
-     * Calculates the average red, green, and blue values of pixels in a given image
-     * @param image: UIImage - the image to be analyzed
-     * @return (red, green, blue) - a triplet of the average red, green, and blue values, respectively
-     * @author Olivia Rorke
-     */
-    func calculateRGB(image:UIImage) -> (red: Int, green: Int, blue: Int) {
-        
-        guard let cgImage = image.cgImage,
-              let cgData = cgImage.dataProvider?.data,
-              let cgBytes = CFDataGetBytePtr(cgData) else {
-            return (0, 0, 0)
-        }
-        let bytesPerPixel = cgImage.bitsPerPixel / cgImage.bitsPerComponent
-        var red, green, blue: Int
-        red = 0
-        green = 0
-        blue = 0
-        
-        let totalPixels = cgImage.width * cgImage.height
-        
-        for x in 0 ..< cgImage.width {
-            for y in 0 ..< cgImage.height {
-                let offset = (y * cgImage.bytesPerRow) + (x * bytesPerPixel)
-                
-                red += Int(cgBytes[offset])
-                green += Int(cgBytes[offset + 1])
-                blue += Int(cgBytes[offset + 2])
-            }
-        }
-        
-        return (red / totalPixels, green / totalPixels, blue / totalPixels)
-    }
-    
-    //creates a SampleData object and populates it with data from the camera
-    //call after taking a photo of either a sample or control
-    func createSampleDataObject(image:UIImage) -> SampleData {
-        let sample = SampleData()
-        
-        let (red, green, blue) = calculateRGB(image:image)
-        sample.setRedValue(redValue:red)
-        sample.setGreenValue(greenValue:green)
-        sample.setBlueValue(blueValue:blue)
-        
-        let intensity = (Double)(red + green + blue)
-        sample.setIntensity(intensity:intensity)
-        
-        sample.calculateColorName()
-        sample.calculateWavelengthValue()
-        
-        return sample
+    func deleteValues(){
+        // calls default values to reset objects to default standing
+        // using only sample one as a temp variable for right now
+        defaultValues(samp: sampleOne)
+        // check if there is a saved data file to delete or not
     }
 }
 
-extension ViewController: AVCapturePhotoCaptureDelegate {
-    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
-        guard let data = photo.fileDataRepresentation() else {
-            return
-        }
-        
-        let image = UIImage(data: data)
-        
-        let sampOne = createSampleDataObject(image: image!)
-        let sampControl = SampleData()
-        sampOne.calculateAbsorbanceValue(control: sampControl)
-        
-        //display data from the camera
-        print("Red: \(sampOne.getRedValue())")
-        print("Green: \(sampOne.getGreenValue())")
-        print("Blue: \(sampOne.getBlueValue())")
-        print("Intensity: \(sampOne.getIntensity())")
-        print("Absorbance: \(sampOne.getAbsorbanceValue())")
-        print("Color name: \(sampOne.getColorName())")
-        print("Wavelength range: \(sampOne.getWavelengthValue())")
-                
-        session?.stopRunning()
-        
-        let imageView = UIImageView(image: image)
-        imageView.contentMode = .scaleAspectFill
-        imageView.frame = view.bounds
-        view.addSubview(imageView)
+protocol dropDownProtocol {
+    func dropDownPressed(string : String)
+}
+    
+    
+class dropDownBtn: UIButton, dropDownProtocol {
+    
+    func dropDownPressed(string: String) {
+        self.setTitle(string, for: .normal)
+        self.dismissDropDown()
     }
+    
+    var dropView = dropDownView()
+    
+    var height = NSLayoutConstraint()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = UIColor.darkGray
+        
+        dropView = dropDownView.init(frame: CGRect.init(x: 0, y: 0, width: 0, height: 0))
+        dropView.delegate = self
+        dropView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    override func didMoveToSuperview() {
+        self.superview?.addSubview(dropView)
+        self.superview?.bringSubviewToFront(dropView)
+        dropView.topAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        dropView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        dropView.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        height = dropView.heightAnchor.constraint(equalToConstant: 0)
+    }
+    
+    var isOpen = false
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if isOpen == false {
+            
+            isOpen = true
+            
+            NSLayoutConstraint.deactivate([self.height])
+            
+            if self.dropView.tableView.contentSize.height > 150 {
+                self.height.constant = 150
+            } else {
+                self.height.constant = self.dropView.tableView.contentSize.height
+            }
+            
+            
+            NSLayoutConstraint.activate([self.height])
+            
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
+                self.dropView.layoutIfNeeded()
+                self.dropView.center.y += self.dropView.frame.height / 2
+            }, completion: nil)
+            
+        } else {
+            isOpen = false
+            
+            NSLayoutConstraint.deactivate([self.height])
+            self.height.constant = 0
+            NSLayoutConstraint.activate([self.height])
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
+                self.dropView.center.y -= self.dropView.frame.height / 2
+                self.dropView.layoutIfNeeded()
+            }, completion: nil)
+            
+        }
+    }
+    
+    func dismissDropDown() {
+        isOpen = false
+        NSLayoutConstraint.deactivate([self.height])
+        self.height.constant = 0
+        NSLayoutConstraint.activate([self.height])
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
+            self.dropView.center.y -= self.dropView.frame.height / 2
+            self.dropView.layoutIfNeeded()
+        }, completion: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class dropDownView: UIView, UITableViewDelegate, UITableViewDataSource  {
+    
+    var dropDownOptions = [String]()
+    
+    var tableView = UITableView()
+    
+    var delegate : dropDownProtocol!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        tableView.backgroundColor = UIColor.darkGray
+        self.backgroundColor = UIColor.darkGray
+        
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(tableView)
+        
+        tableView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dropDownOptions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = dropDownOptions[indexPath.row]
+        cell.backgroundColor = UIColor.darkGray
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.delegate.dropDownPressed(string: dropDownOptions[indexPath.row])
+        self.tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 }
