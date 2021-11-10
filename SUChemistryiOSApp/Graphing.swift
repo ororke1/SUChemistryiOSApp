aec376c9aa4d6631c9911cc43678e87b79462520
Olivia Rorke <ororke1@ac102-08-00952.salisbury.edu>
2021-11-10 13:15:18 -0500

    Added UI placeholder, graphing skeleton, and full camera functionality

---
+++ SUChemistryiOSApp/Graphing.swift
@@ -0,0 +1,29 @@
+//
+//  Graphing.swift
+//  SUChemistryiOSApp
+//
+//  Created by Olivia Rorke on 10/25/21.
+//
+
+import UIKit
+
+class Graphing: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
