1572beef9b107f32392ba789854b30c03d65c308
Olivia Rorke <ororke1@ac102-03-00950.salisbury.edu>
2021-11-15 15:46:53 -0500

    Added Main storyboard back

--- SUChemistryiOSApp/CameraViewController.swift
+++ SUChemistryiOSApp/CameraViewController.swift
@@ -2,28 +2,95 @@
 //  CameraViewController.swift
 //  SUChemistryiOSApp
 //
-//  Created by Olivia Rorke on 11/8/21.
-//
 
+import AVFoundation
 import UIKit
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+@IBOutlet weak var imageView: UIImageView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+let imagePicker = UIImagePickerController()
 
-        // Do any additional setup after loading the view.
+override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .black
+    imagePicker.delegate = self
+    imagePicker.allowsEditing = true
+
+}
+
+    @IBAction func toGraph(_ sender: Any) {
+        performSegue(withIdentifier: "segueToGraph", sender: self)
     }
     
+@IBAction func didTapAddPhotoButton(_ sender: Any) {
+    
+    let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+    
+    alert.addAction(UIAlertAction(title: "Photo Gallery", style: .default, handler: { (button) in
+        self.imagePicker.sourceType = .photoLibrary
+        self.present(self.imagePicker, animated: true, completion: nil)
+    }))
+    
+    alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (button) in
+        self.imagePicker.sourceType = .camera
+        self.present(self.imagePicker, animated: true, completion: nil)
+    }))
+    
+    alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+    
+    present(alert, animated: true, completion: nil)
+}
 
-    /*
-    // MARK: - Navigation
+func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    guard let pickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else { return }
+    imageView.image = pickedImage
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+            
+            let (red, green, blue) = calculateRGB(image: pickedImage)
+            print(red)
+            print(green)
+            print(blue)
+    dismiss(animated: true, completion: nil)
+    
+            
+    
+}
+
+
+/**
+ * Calculates the average red, green, and blue values of pixels in a given image
+ * @param image: UIImage - the image to be analyzed
+ * @return (red, green, blue) - a triplet of the average red, green, and blue values, respectively
+ * @author Olivia Rorke
+ */
+func calculateRGB(image:UIImage) -> (red: Int, green: Int, blue: Int) {
+    
+    guard let cgImage = image.cgImage,
+          let cgData = cgImage.dataProvider?.data,
+          let cgBytes = CFDataGetBytePtr(cgData) else {
+        return (0, 0, 0)
     }
-    */
+    let bytesPerPixel = cgImage.bitsPerPixel / cgImage.bitsPerComponent
+    var red, green, blue: Int
+    red = 0
+    green = 0
+    blue = 0
+    
+    let totalPixels = cgImage.width * cgImage.height
+    
+    for x in 0 ..< cgImage.width {
+        for y in 0 ..< cgImage.height {
+            let offset = (y * cgImage.bytesPerRow) + (x * bytesPerPixel)
+            
+            red += Int(cgBytes[offset])
+            green += Int(cgBytes[offset + 1])
+            blue += Int(cgBytes[offset + 2])
+        }
+    }
+    
+    return (red / totalPixels, green / totalPixels, blue / totalPixels)
+}   
 
 }
--- SUChemistryiOSApp/Graphing.swift
+++ SUChemistryiOSApp/Graphing.swift
@@ -1,29 +1,84 @@
 //
-//  Graphing.swift
-//  SUChemistryiOSApp
-//
-//  Created by Olivia Rorke on 10/25/21.
+//  GraphingViewController.swift
+//  
 //
 
 import UIKit
+import Charts
+import TinyConstraints
 
-class Graphing: UIViewController {
+class GraphingViewController: UIViewController, ChartViewDelegate {
+    
+    @IBAction func closeGraphing(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    lazy var lineChartView: LineChartView = {
+        let chartView = LineChartView()
+        chartView.backgroundColor = .systemBlue
+        
+        chartView.rightAxis.enabled = false
+        
+        let yAxis = chartView.leftAxis
+        yAxis.labelFont = .boldSystemFont(ofSize: 12)
+        yAxis.setLabelCount(6, force: false)
+        yAxis.labelTextColor = .white
+        yAxis.axisLineColor = .white
+        yAxis.labelPosition = .outsideChart
+        
+        chartView.xAxis.labelPosition = .bottom
+        chartView.xAxis.labelFont = .boldSystemFont(ofSize: 12)
+        chartView.xAxis.setLabelCount(6, force: false)
+        chartView.xAxis.labelTextColor = .white
+        chartView.xAxis.axisLineColor = .systemBlue
+        
+        chartView.animate(xAxisDuration: 2.5)
+        
+        return chartView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(lineChartView)
+        lineChartView.centerInSuperview()
+        lineChartView.width(to: view)
+        lineChartView.heightToWidth(of: view)
+        
+        setData()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
+        print(entry)
     }
-    */
+    
+    func setData() {
+        let set1 = LineChartDataSet(entries: yValues, label: "Concentration")
+        
+        set1.mode = .cubicBezier
+        set1.drawCirclesEnabled = false
+        set1.lineWidth = 3
+        set1.setColor(.white)
+        //set1.fill = Fill(color: .white)
+        set1.fillAlpha = 0.0
+        set1.drawFilledEnabled = true
+        
+        set1.drawHorizontalHighlightIndicatorEnabled = false
+        set1.highlightColor = .systemRed
+        
+        let data = LineChartData(dataSet: set1)
+        data.setDrawValues(false)
+        lineChartView.data = data
+        
+    }
+    
+    let yValues: [ChartDataEntry] = [
+        ChartDataEntry(x: 0.1, y: 155),
+        ChartDataEntry(x: 0.3, y: 176),
+        ChartDataEntry(x: 0.4, y: 166)
+    
+    ]
+    
 
 }
